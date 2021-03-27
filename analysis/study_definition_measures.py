acd35e875dc79fcfc10584f7526666e73d8e9c18
Alex Walker <alex.walker@thedatalab.org>
2021-03-27 13:45:12 +0000

    simpler count and fix graphs

--- analysis/study_definition_measures.py
+++ analysis/study_definition_measures.py
@@ -7,7 +7,6 @@ from cohortextractor import (
     combine_codelists,
     filter_codes_by_category,
 )
-from codelists import covid_codelist
 
 csvt_codes = codelist(["G08X", "I636", "I676"], system="icd10")
 
@@ -18,72 +17,31 @@ study = StudyDefinition(
         "incidence": 0.05,
     },
     index_date="2019-02-01",
-    population=patients.satisfying(
-        """
-            has_follow_up
-        AND (age >=18 AND age <= 110)
-        """,
-        has_follow_up=patients.registered_with_one_practice_between(
-            "index_date - 1 year", "index_date"
-        ),
-        age=patients.age_as_of(
-            "index_date",
-            return_expectations={
-                "rate": "universal",
-                "int": {"distribution": "population_ages"},
-            },
-        ),
+    population=patients.all(),
+    CSVT_G08X=patients.admitted_to_hospital(
+        with_these_diagnoses=codelist(["G08X"], system="icd10"),
+        between=["index_date", "last_day_of_month(index_date)"],
+        return_expectations={"incidence": 0.05},
     ),
-    covid_hospitalisation=patients.categorised_as(
-        {
-            "COVID-19 positive": "covid_positive AND NOT covid_hospitalised",
-            "COVID-19 hospitalised": "covid_hospitalised",
-            "General population": "DEFAULT",
-        },
-        return_expectations={
-            "incidence": 1,
-            "category": {
-                "ratios": {
-                    "COVID-19 positive": 0.1,
-                    "COVID-19 hospitalised": 0.1,
-                    "General population": 0.8,
-                }
-            },
-        },
-        covid_positive=patients.with_test_result_in_sgss(
-            pathogen="SARS-CoV-2",
-            test_result="positive",
-            between=["2020-01-01", "last_day_of_month(index_date)"],
-            date_format="YYYY-MM-DD",
-            return_expectations={"date": {"earliest": "index_date"}},
-        ),
-        covid_hospitalised=patients.admitted_to_hospital(
-            with_these_diagnoses=covid_codelist,
-            between=["2020-01-01", "last_day_of_month(index_date)"],
-            return_expectations={"incidence": 0.20},
-        ),
-    ),
-    CSVT=patients.satisfying(
-        "CSVT_hospital OR CSVT_ons",
-        CSVT_hospital=patients.admitted_to_hospital(
-            with_these_diagnoses=csvt_codes,
-            between=["index_date", "last_day_of_month(index_date)"],
-            return_expectations={"incidence": 0.05},
-        ),
-        CSVT_ons=patients.with_these_codes_on_death_certificate(
-            csvt_codes,
-            between=["index_date", "last_day_of_month(index_date)"],
-            return_expectations={"incidence": 0.05},
-        ),
+    CSVT_I636_I676=patients.admitted_to_hospital(
+        with_these_diagnoses=codelist(["I636", "I676"], system="icd10"),
+        between=["index_date", "last_day_of_month(index_date)"],
+        return_expectations={"incidence": 0.05},
     ),
 )
 
 
 measures = [
     Measure(
-        id="CSVT_rate",
-        numerator="CSVT",
+        id="CSVT_G08X_rate",
+        numerator="CSVT_G08X",
         denominator="population",
-        group_by=["covid_hospitalisation"],
+        group_by="population",
+    ),
+    Measure(
+        id="CSVT_I636_I676_rate",
+        numerator="CSVT_I636_I676",
+        denominator="population",
+        group_by="population",
     ),
 ]
--- analysis/time_series_plots.py
+++ analysis/time_series_plots.py
@@ -14,19 +14,15 @@ def import_timeseries():
     table = table.set_index(["date"] + m.group_by)
     table = redact_small_numbers(table)
     table = table.unstack(m.group_by)
-    table[f"total_{m.numerator}"] = table[m.numerator].sum(axis=1)
-    table[f"total_{m.denominator}"] = table[m.denominator].sum(axis=1)
     table.to_csv(f"output/table_{m.id}.csv")
     out = table[m.numerator]
-    totals = table[f"total_{m.numerator}"]
     # Return columns in reverse order
-    return out.iloc[:, ::-1], totals
+    return out.iloc[:, ::-1], out.iloc[:, ::-1]
 
 
 def redact_small_numbers(df):
     mask_n = df[m.numerator].isin([1, 2, 3, 4, 5])
-    mask_d = df[m.denominator].isin([1, 2, 3, 4, 5])
-    mask = mask_n | mask_d
+    mask = mask_n
     df.loc[mask, :] = np.nan
     return df
 
@@ -60,25 +56,28 @@ def graphing_options(column):
     ax.set_ylabel(f"people {grammar_decider(column)}")
     handles, labels = ax.get_legend_handles_labels()
     handles, labels = list(reversed(handles)), list(reversed(labels))
-    ax.legend(handles, labels, loc=3, prop={"size": 9}).set_title("")
+    ax.get_legend().remove()
     ax.set_xticklabels(map(line_format, df.index), rotation="horizontal")
     for (n, l) in enumerate(ax.xaxis.get_ticklabels()):
-        if (n > 0) and ((n + 1) % 2 != 0):
+        if (n > 0) and ((n) % 4 != 0):
             l.set_visible(False)
     ax.xaxis.label.set_visible(False)
     plt.tight_layout()
 
 
-m = measures[0]
-df, totals = import_timeseries()
-ax = df.plot(
-    kind="bar",
-    stacked=True,
-    width=0.85,
-    alpha=0.9,
-    color=["#176dde", "#e6e600", "#ffad33"],
-)
-totals.plot(kind="bar", ax=ax, yerr=get_ci(totals), alpha=0, label="_nolegend_")
-title = f"People {grammar_decider(m.numerator)} each month:"
-graphing_options(m.numerator)
+fig, axes = plt.subplots(ncols=2, nrows=1, sharey=False, figsize=[10, 4])
+for i, ax in enumerate(axes.flat):
+    m = measures[i]
+    df, totals = import_timeseries()
+    df.plot(
+        kind="bar",
+        stacked=True,
+        ax=ax,
+        width=0.85,
+        alpha=0.9,
+        color=["#176dde", "#e6e600", "#ffad33"],
+    )
+    totals.plot(kind="bar", ax=ax, yerr=get_ci(totals), alpha=0, label="_nolegend_")
+    title = f"{chr(97 + i)}) People {grammar_decider(m.numerator)} each month:"
+    graphing_options(m.numerator)
 plt.savefig("output/event_count_time_series.svg")
